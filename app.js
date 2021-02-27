ac44ecc988afca609d5978f3b1fb1695af70e9a1
Lenka Shynkarova <lllee@post.cz>
2021-02-27 17:09:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+PRAVIDLA HRY:
+
+- Hra má dva hráče, kteří se střídají každé kolo
+- V každé kole hází hráč kostkou kolikrát chce. Hodnota každého hodu se přičítá k jeho bodů v daném kole.
+- Pokud na kostce padne 1, ztrácí všechny body v daném kole a na řadu se dostává hráč dvě.
+- Hráč může zvolit "dost", což znamená, že všechny body nahrané v jeho kole se přičtou k jeho celkovým bodům. Poté je na řadě hráč dvě.
+- Hra končí jakmile jeden z hráčů dosáhne dopředu určeného počtu bodů (typicky 100 bodů).
+
+*/
